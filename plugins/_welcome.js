94048b48908b1cc278b406a16b120b3a55993eec
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-22 02:19:56 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,8 +7,8 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
-  let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
+  let txt = '「⭐」New Member「⭐」'
+  let txt1 = '「⭐」Bye Member「⭐」'
   let groupSize = participants.length
   if (m.messageStubType == 27) {
     groupSize++;
@@ -17,11 +17,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    let bienvenida = `「⭐」 *Bienvenido* a ${groupMetadata.subject}\n「⭐」@${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n「⭐」 Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✎ Puedes usar *#help* para ver la lista de comandos.`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    let bye = `「⭐」 *Adiós* de ${groupMetadata.subject}\n「⭐」@${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n「⭐」 Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✎ Puedes usar *#help* para ver la lista de comandos.`
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
   }}
